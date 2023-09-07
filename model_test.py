5ecf210413729c76fc7c478260e762f50c3ebf69
thesaiedfs <kalidindisaikiran09@gmail.com>
2023-09-07 18:08:12 +0530

    modified model

--- model_test.py
+++ model_test.py
@@ -2,15 +2,13 @@ from gensim.models import Word2Vec
 
 model = Word2Vec.load('word2vec2.model')
 
-sim_skills = model.wv.most_similar('python', topn=10)
-similar_skills = []
-# similar_skills = [skill for skill, _ in sim_skills]
-for skills_tuple in sim_skills:
-    skill = skills_tuple[0]
-    score = str(skills_tuple[1])
-    skills_ = skill+'|'+score
-    similar_skills.append(skills_)
-print(similar_skills)
+sim_skills = model.wv.most_similar('java', topn=10)
+
+similar_skills = [skill for skill, _ in sim_skills]
+
+# for skill in similar_skills:
+#     simi_skill = model.wv.most_similar(skill, topn=10)
+#     print(simi_skill)
 
 
 
